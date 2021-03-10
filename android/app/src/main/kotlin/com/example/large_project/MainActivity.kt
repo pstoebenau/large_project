6cb202bd005fe87972b25e9fb601e8114a3217d1
Patrick Stoebenau <pstoebenau@knights.ucf.edu>
2021-03-10 00:50:37 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/large_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.large_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/hot_or_not.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Swiper extends StatelessWidget {
+  const Swiper({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Hot or Not"),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Swipe away on code!',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
