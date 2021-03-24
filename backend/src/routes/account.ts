f331c2b2d97f07a69aa08950bd1a5859c2460bdd
Caroline Nejedlo <cnnejedlo@yahoo.com>
2021-03-24 17:49:15 -0400

    signup
    start of signup api

---
+++ backend/src/routes/account.ts
@@ -0,0 +1,37 @@
+import express, { NextFunction, Request, Response } from "express";
+import User from "@models/user";
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+router.post("/signup", async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.body) {
+      return res.status(500).json({
+        message: "Invalid input",
+      });
+    }
+
+    let { firstName, lastName, email, username, password } = req.body;
+
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      username,
+      password,
+    });
+
+    try {
+      let result = await user.save();
+
+      return res.status(201).json({
+        user: result,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: error.message,
+        error,
+      });
+    }
+  }
+);
