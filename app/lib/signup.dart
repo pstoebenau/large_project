bc4ab1868cb9849ff1666b9a561a3b1d01453a22
Joe Brashear <joebrashear@live.com>
2021-03-30 17:17:27 -0400

    Added a way for the swiping feature to be navigated to by pushing to the next page when clicking login

---
+++ app/lib/signup.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import './swiping.dart';
+
+class Signup extends StatelessWidget {
+  const Signup();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            SizedBox(height: 50),
+            new Container(
+              child: new Image.asset(
+                "assets/chili.png",
+                width: 150,
+                height: 150,
+                fit: BoxFit.cover
+              ),
+            ),
+            SizedBox(height: 40),
+            Text(
+              'LOGIN',
+              style: TextStyle(
+                fontSize: 32,
+                letterSpacing: 15 
+              ),
+            ),
+            SizedBox(height: 60),
+            new Container(
+              width: 350,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  TextField(
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ]
+              ),
+            ),
+            SizedBox(height: 60),
+            SizedBox(
+              width: 220,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: ()=>{
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SwipingPage(),
+                    ),
+                  ),
+                },
+                child: Text('Login'),
+              ),
+            ),
+            SizedBox(height: 60),
+            Text(
+              "Forgot Password?",
+              style: TextStyle(decoration: TextDecoration.underline),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Or create a new account",
+              style: TextStyle(decoration: TextDecoration.underline),
+            ),
+            SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
+  }
+}
