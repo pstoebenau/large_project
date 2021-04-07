9041a71e25739059e1a65b12c687e1461a200651
Caroline Nejedlo <cnnejedlo@yahoo.com>
2021-04-06 22:34:47 -0400

    some login stuff

--- backend/src/routes/account.ts
+++ backend/src/routes/account.ts
@@ -74,44 +74,6 @@ router.post(
   }
 );
 
-router.get("/send", async (req: Request, res: Response, next: NextFunction) => {
-  var smtpConfig = {
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
-    auth: {
-        user: 'mmidnight.magic@gmail.com',
-        pass: 'magic!password'
-    }
-  };
-
-  var transporter = nodemailer.createTransport(smtpConfig);
-
-  let token = jwt.sign({userId: "this-is-the-user-id-string"}, config.server.secret);
-
-  let verifyLink = `http://${config.server.hostname}:${config.server.port}/api/account/verify/token=${token}`;
-
-  try {
-    let send = await transporter.sendMail({
-      from: '"Chill Midnight 👻" <mmidnight.magic@gmail.com>', // sender address
-      to: "joxahec390@shzsedu.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: `Hello,<br> Please Click on the link to verify your email.<br><a href=${verifyLink}>Click here to verify</a>`, // html body
-    });
-
-    console.log("Message sent: %s", send.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(send));
-
-    return res.status(200).json();
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-      error,
-    });
-  }
-});
-
 router.get("/verify/:token", async function (req, res) {
   let token = req.params.token;
 
@@ -120,13 +82,32 @@ router.get("/verify/:token", async function (req, res) {
     let userId = data.userId;
   
     let user = await User.findById(userId).exec();
+    user?.updateOne({active: true}, null, (err, res) => {
+
+    });
     console.log(user);
 
     res.status(200).json({user});
   } catch (error) {
     res.status(500).json({
       message: error.message
-    })
+    });
+  }
+});
+
+router.post('/login', async function(req,res) {
+  let { username, password} = req.body;
+  const hash = bcrypt.hash(password, 10);
+  
+  try {
+    let user = await User.findOne({ username });
+    console.log(user);
+
+    res.status(200).json();
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
   }
 });
 
