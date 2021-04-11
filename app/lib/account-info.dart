76adb9cecff586e232a4510048d0e6ecb3a218af
Joe Brashear <joebrashear@live.com>
2021-04-11 12:44:06 -0400

    Started putting the images together, going for lunch now tho

--- app/lib/account-info.dart
+++ app/lib/account-info.dart
@@ -18,19 +18,38 @@ class AccountInfoPage extends StatelessWidget {
                   // This is the profile picture
                   Align(
                     alignment: Alignment(0, -0.6),
-                    child: new Container(
-                      child: new Image.asset(
+                    child: Container(
+                      child: Image.asset(
                         "assets/Rectangle 27.png",
                         width: 550,
-                        height: 500,
+                        height: 515,
                       ),
                     ),
                   ),
                   Align(
-                    alignment: Alignment(-0.8, -1),
-                    child: new Container(
-                      child: new Image.asset("assets/joe.png",
+                    alignment: Alignment(-0.9, -1),
+                    child: Container(
+                      child: Image.asset("assets/joe.png",
                           width: 90, height: 90, fit: BoxFit.cover),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(.6, -.88),
+                    child: Container(
+                      child: Image.asset("assets/edit.png",
+                          width: 30, height: 30, fit: BoxFit.cover),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(0, -.88),
+                    child: Container(
+                      child: Text( 'Joe Mama', style: TextStyle(fontSize: 25),),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(-.2, -.7),
+                    child: Container(
+                      child: Text( 'username: Joe_Mama', style: TextStyle(fontSize: 15),),
                     ),
                   ),
                 ],
