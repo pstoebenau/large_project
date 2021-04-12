e623ae3db68346c51fbd5ca3aadaa08da79ccb80
Joe Brashear <joebrashear@live.com>
2021-04-12 11:02:43 -0400

    Added some more things

--- app/lib/account-info.dart
+++ app/lib/account-info.dart
@@ -15,9 +15,9 @@ class AccountInfoPage extends StatelessWidget {
               height: 600,
               child: Stack(
                 children: <Widget>[
-                  // This is the profile picture
+                  // This is the rectangle background
                   Align(
-                    alignment: Alignment(0, -0.6),
+                    alignment: Alignment(0, -0.1),
                     child: Container(
                       child: Image.asset(
                         "assets/Rectangle 27.png",
@@ -27,29 +27,80 @@ class AccountInfoPage extends StatelessWidget {
                     ),
                   ),
                   Align(
-                    alignment: Alignment(-0.9, -1),
+                    alignment: Alignment(-.85, -1),
                     child: Container(
                       child: Image.asset("assets/joe.png",
                           width: 90, height: 90, fit: BoxFit.cover),
                     ),
                   ),
-                  Align(
-                    alignment: Alignment(.6, -.88),
-                    child: Container(
-                      child: Image.asset("assets/edit.png",
-                          width: 30, height: 30, fit: BoxFit.cover),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment(.6, -.85),
+                      child: Container(
+                        child: Image.asset("assets/edit.png",
+                            width: 30, height: 30, fit: BoxFit.cover),
+                      ),
                     ),
                   ),
                   Align(
-                    alignment: Alignment(0, -.88),
+                    alignment: Alignment(0, -.85),
                     child: Container(
-                      child: Text( 'Joe Mama', style: TextStyle(fontSize: 25),),
+                      child: Text(
+                        'Joe Mama',
+                        style: TextStyle(fontSize: 25),
+                      ),
                     ),
                   ),
                   Align(
-                    alignment: Alignment(-.2, -.7),
+                    alignment: Alignment(-.45, -.62),
                     child: Container(
-                      child: Text( 'username: Joe_Mama', style: TextStyle(fontSize: 15),),
+                      child: Text(
+                        'username: Joe_Mama',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(-.43, -.5),
+                    child: Container(
+                      child: Text(
+                        'email: joe@mama.com',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment(0, -.2),
+                      child: Container(
+                        child: Image.asset("assets/Rectangle 28.png",
+                            width: 287, height: 110),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(-.1, -.28),
+                    child: Container(
+                      child: Text(
+                        'About Me: You\'ve never had it Joe good',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(.45, .08),
+                    child: Container(
+                      child: Text(
+                        'Your Snippets                + ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(0, -.2),
+                    child: Container(
+                      child: Image.asset("assets/Rectangle 28.png",
+                          width: 287, height: 110),
                     ),
                   ),
                 ],
