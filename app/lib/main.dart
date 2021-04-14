d7a142f69a31c3363ce1620007b7a774808c2542
jennysoto1424 <soto.jenny24@gmail.com>
2021-04-13 21:14:56 -0400

    Use only for forgot password

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
+import "forgot_pass.dart";
 
 void main() {
   runApp(MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         accentColor: Color(0xff003EF8),
         brightness: Brightness.dark,
       ),
-      home: Login(),
+      home: ForgotPassword(),
     );
   }
 }
