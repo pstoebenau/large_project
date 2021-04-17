2c1c885b6b16eb81effc262d3a7a6899f84fd5be
Patrick Stoebenau <pstoebenau@knights.ucf.edu>
2021-04-17 01:59:32 -0400

    Added tests and fixed login api endpoint

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -28,7 +28,7 @@ const MONGO = {
   url: mongoUrl
 }
 
-const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
+const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'http://localhost';
 const SERVER_PORT = process.env.PORT || 8080;
 const SERVER_SECRET = "verysecretwow";
 
