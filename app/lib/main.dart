3e541b3e2b1c4eecdada92755a2881071165460e
Jacob <jlfeigenheimer@gmail.com>
2021-04-18 17:05:41 -0400

    Changed the homepage back to login.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'login.dart';
-import 'landing.dart';
+import 'login.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         accentColor: Color(0xff003EF8),
         brightness: Brightness.dark,
       ),
-      home: Landing(),
+      home: Login(),
     );
   }
 }
