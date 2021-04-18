f04486b307f35f472de6eb36ee09b77cf9153484
cnnejedlo <carly@Carly-Desktop.localdomain>
2021-04-18 01:28:12 -0400

    added profile image and about to user

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -2,10 +2,12 @@ import { Document } from 'mongoose';
 
 export default interface User extends Document {
   userId: string;
+  profileImage: string;
   firstName: string;
   lastName: string;
   email: string;
   username: string;
   password: string;
+  about: string;
   active: boolean;
 }
--- backend/src/routes/account.ts
+++ backend/src/routes/account.ts
@@ -55,11 +55,13 @@ router.post(
     const hash = await bcrypt.hash(password, 10);
 
     const user = new User({
+      profileImage: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
       firstName,
       lastName,
       email,
       username,
       password: hash,
+      about: "",
       active: false,
     });
 
