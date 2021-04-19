b03ce51eb738b1df37a83b44eee9efb325260a95
Patrick Stoebenau <pstoebenau@knights.ucf.edu>
2021-04-19 00:59:43 -0400

    Login now works with form

--- app/lib/login.dart
+++ app/lib/login.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
-
+import 'package:alert_dialog/alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:http/http.dart';
 import 'package:provider/provider.dart';
 import 'package:large_project/models/userInfo.dart';
-
 import 'globals.dart';
 
 class Login extends StatefulWidget {
@@ -26,12 +25,22 @@ class _LoginState extends State<Login> {
   }
 
   void login() async {
+    final Map<String, dynamic> formData = _formKey.currentState.value;
+
     var url = Uri.parse('${Globals.apiUrl}/api/account/login');
-    var response = await post(url, body: {
-      "username": "namejeff6",
-      "password": "password"
-    });
-    userInfo.token = json.decode(response.body)['token'];
+    var response = await post(url, body: formData);
+
+    if (response.statusCode != 200)
+      return;
+
+    final resObj = json.decode(response.body);
+    if (resObj['message'] == 'success') {
+      userInfo.token = resObj['token'];
+      Navigator.pushReplacementNamed(context, '/swipe');
+    }
+    else {
+      return alert(context, content: Text(resObj['message']));
+    }
   }
 
   @override
@@ -39,74 +48,85 @@ class _LoginState extends State<Login> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            SizedBox(height: 50),
-            new Container(
-              child: new Image.asset(
-                "assets/chili.png",
-                width: 150,
-                height: 150,
-                fit: BoxFit.cover
+        child: Container(
+          width: MediaQuery.of(context).size.width*0.8,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Spacer(),
+              Container(
+                child: new Image.asset(
+                  "assets/chili.png",
+                  width: 150,
+                  height: 150,
+                  fit: BoxFit.cover
+                ),
               ),
-            ),
-            SizedBox(height: 40),
-            Text(
-              'LOGIN',
-              style: TextStyle(
-                fontSize: 32,
-                letterSpacing: 15 
+              SizedBox(height: 40),
+              Text(
+                'LOGIN',
+                style: TextStyle(
+                  fontSize: 32,
+                  letterSpacing: 15 
+                ),
               ),
-            ),
-            SizedBox(height: 40),
-            FormBuilder(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Username',
+              SizedBox(height: 40),
+              FormBuilder(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FormBuilderTextField(
+                      name: 'username',
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Username',
+                      ),
+                      validator: FormBuilderValidators.required(context),
                     ),
-                  ),
-                  SizedBox(height: 50),
-                  TextField(
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
+                    SizedBox(height: 50),
+                    FormBuilderTextField(
+                      name: 'password',
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                      validator: FormBuilderValidators.required(context),
                     ),
-                  ),
-                  SizedBox(
-                    width: 220,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        login();
-                        Navigator.pushReplacementNamed(context, '/swipe');
-                      },
-                      child: Text('Login'),
+                    SizedBox(height:50),
+                    SizedBox(
+                      width: 220,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (!_formKey.currentState.validate())
+                            return;
+
+                          _formKey.currentState.save();
+                          login();
+                        },
+                        child: Text('Login'),
+                      ),
                     ),
-                  ),
-                ]
+                  ]
+                ),
               ),
-            ),
-            SizedBox(height: 60),
-            Text(
-              "Forgot Password?",
-              style: TextStyle(decoration: TextDecoration.underline),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "Or create a new account",
-              style: TextStyle(decoration: TextDecoration.underline),
-            ),
-            SizedBox(height: 20),
-          ],
+              Spacer(flex: 2),
+              Text(
+                "Forgot Password?",
+                style: TextStyle(decoration: TextDecoration.underline),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Or create a new account",
+                style: TextStyle(decoration: TextDecoration.underline),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
