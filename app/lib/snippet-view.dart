80daa5577dac6055418f0181c8e985f4d58f927d
Joe Brashear <joebrashear@live.com>
2021-04-18 20:37:21 -0400

    Fixed up everything, ready to for pull request

--- app/lib/snippet-view.dart
+++ app/lib/snippet-view.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import 'models/snippet.dart';
+// import 'models/snippet.dart';
 
-class SnippetView extends StatefulWidget {
-  final Snippet snippet;
-  SnippetView({Key key, this.snippet}) : super(key: key);
-  @override
-  _SnippetViewState createState() => _SnippetViewState(snippet: snippet);
-}
+// class SnippetView extends StatefulWidget {
+//   final Snippet snippet;
+//   SnippetView({Key key, this.snippet}) : super(key: key);
+//   @override
+//   _SnippetViewState createState() => _SnippetViewState(snippet: snippet);
+// }
 
-class _SnippetViewState extends State<SnippetView> {
+// class _SnippetViewState extends State<SnippetView> {
 
-  _SnippetViewState(this.snippet, {Object snippet});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
+//   _SnippetViewState(this.snippet, {Object snippet});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
       
-    );
-  }
-}
+//     );
+//   }
+// }
