b5eb70e96a0a32ea7f1ccdcf43901bdbf4fc5cdb
Patrick Stoebenau <pstoebenau@knights.ucf.edu>
2021-04-19 02:15:44 -0400

    Create snippet makes image from text now

--- backend/src/routes/snippets.ts
+++ backend/src/routes/snippets.ts
@@ -4,6 +4,10 @@ import Snippet from "@/schemas/snippets";
 import mongoose from "mongoose";
 import config from "@/config/config";
 import jwt from "jsonwebtoken";
+import fs from "fs";
+import crypto from "crypto";
+import { execSync } from "child_process";
+import snippets from "@/schemas/snippets";
 
 // /api/snippet/deleteSnippet
 
@@ -23,22 +27,45 @@ import jwt from "jsonwebtoken";
 // }
 // returns the snippet that was deleted.
 
+// /api/snippet/create
+// input
+// {
+//   "token": string,
+//   "codeText": string
+// }
+// output
+// {
+//   "snippet": Snippet,
+//   "message": string,
+// }
+
 const router = express.Router();
 
 router.post(
   "/create",
   async (req: Request, res: Response, next: NextFunction) => {
-    let { token, imageURL } = req.body;
-
-    const data = jwt.verify(token, config.server.secret) as Token;
-
-    const snippet = new Snippet({
-      userId: data.userId,
-      imageURL,
-      score: 0,
-    });
-
     try {
+      let { token, codeText } = req.body;
+
+      const data = jwt.verify(token, config.server.secret) as Token;
+
+      // Verify code length
+      if (codeText.length >= 1000)
+        return res.status(400).json({message: "Snippet too long"});
+
+      // Use carbon-now-cli to convert text to image
+      const dir = "src/public/uploads/";
+      const uuid = crypto.randomBytes(16).toString("hex");
+      fs.writeFileSync(dir + uuid, codeText);
+      execSync(`npx carbon-now ${dir}${uuid} -t ${dir}${uuid} -h --config carbon-now.json`);
+      fs.unlinkSync(dir + uuid);
+
+      const snippet = new Snippet({
+        userId: data.userId,
+        imageURL: `${config.server.url}/uploads/${uuid}.png`,
+        score: 0,
+      });
+
       let results = await snippet.save();
 
       return res.status(200).json({
