4d8c22b49f58487a53366b7d7ebcee5049e69a60
Smercado0935 <Mercados@knights.ucf.edu>
2021-04-19 20:24:04 -0400

    updated unit testing

--- backend/src/tests/account.test.ts
+++ backend/src/tests/account.test.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import config from '../config/config';
+import jwt from "jsonwebtoken";
 
-const url = config.server.hostname+":"+config.server.port;
+const url = config.server.hostname + ":" + config.server.port;
 
 test('Create account', async () => {
   let randNum = Math.ceil(Math.random() * 99999);
@@ -41,7 +42,24 @@ test('Login to account', async () => {
 test('Forgot password', async () => {
   let randNum = Math.ceil(Math.random() * 99999);
 
-  let res = await axios.post(`${url}/api/account/forgotpassword`, {
+  let res = await axios.post(`${url}/api/account/signup`, {
+    firstName: "name",
+    lastName: "jeff",
+    email: `namejeff${randNum}@mailinator.com`,
+    username: `namejeff${randNum}`,
+    password: "password"
+  });
+
+  expect(res.data.message).toBe("success");
+
+  res = await axios.post(`${url}/api/account/login`, {
+    "username": `namejeff${randNum}`,
+    "password": "password"
+  });
+
+  expect(res.data.message).toBe("verify email");
+
+  res = await axios.post(`${url}/api/account/forgotpassword`, {
     email: `namejeff${randNum}@mailinator.com`
   });
 
@@ -51,10 +69,29 @@ test('Forgot password', async () => {
 test('Change password', async () => {
   let randNum = Math.ceil(Math.random() * 99999);
 
-  let res = await axios.post(`${url}/api/account/changepassword`, {
+  let res = await axios.post(`${url}/api/account/signup`, {
+    firstName: "name",
+    lastName: "jeff",
+    email: `namejeff${randNum}@mailinator.com`,
+    username: `namejeff${randNum}`,
+    password: "password"
+  });
+
+  expect(res.data.message).toBe("success");
+
+  res = await axios.post(`${url}/api/account/login`, {
+    "username": `namejeff${randNum}`,
+    "password": "password"
+  });
+
+  expect(res.data.message).toBe("verify email");
+
+  let token = jwt.sign({ userId: `namejeff${randNum}` }, config.server.secret);
+
+  res = await axios.post(`${url}/api/account/changepassword`, {
     password: "newpassword",
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MDdhMmY5NmVjOGU5ZTAwMmJlNGMxMTIiLCJmaXJzdG5hbWUiOiJTaGFseW5uIiwidXNlcm5hbWUiOiJNZXJjYWRvIiwiaWF0IjoxNjE4NjIwNDgxfQ.89a-kWSz5fr587osiGQ7kroabDzsxkpA87s-CvRmpLM"
-});
+    token: token
+  });
 
   expect(res.data.message).toBe("success");
 });
@@ -62,9 +99,28 @@ test('Change password', async () => {
 test('Create snippet', async () => {
   let randNum = Math.ceil(Math.random() * 99999);
 
-  let res = await axios.post(`${url}/api/snippet/create`, {
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MDdhMmY5NmVjOGU5ZTAwMmJlNGMxMTIiLCJmaXJzdG5hbWUiOiJTaGFseW5uIiwidXNlcm5hbWUiOiJNZXJjYWRvIiwiaWF0IjoxNjE4NjIwNDgxfQ.89a-kWSz5fr587osiGQ7kroabDzsxkpA87s-CvRmpLM",
-    imageURL: "image.jpeg"
+  let res = await axios.post(`${url}/api/account/signup`, {
+    firstName: "name",
+    lastName: "jeff",
+    email: `namejeff${randNum}@mailinator.com`,
+    username: `namejeff${randNum}`,
+    password: "password"
+  });
+
+  expect(res.data.message).toBe("success");
+
+  res = await axios.post(`${url}/api/account/login`, {
+    "username": `namejeff${randNum}`,
+    "password": "password"
+  });
+
+  expect(res.data.message).toBe("verify email");
+
+  let token = jwt.sign({ userId: `namejeff${randNum}` }, config.server.secret);
+
+  res = await axios.post(`${url}/api/snippet/create`, {
+    token: token,
+    imageURL: "image"
   });
 
   expect(res.data.message).toBe("success");
@@ -73,8 +129,34 @@ test('Create snippet', async () => {
 test('Delete snippet', async () => {
   let randNum = Math.ceil(Math.random() * 99999);
 
-  let res = await axios.post(`${url}/api/snippet/deleteSnippet`, {
-		_id: "607c94b717e587002cabadfd"
+  let res = await axios.post(`${url}/api/account/signup`, {
+    firstName: "name",
+    lastName: "jeff",
+    email: `namejeff${randNum}@mailinator.com`,
+    username: `namejeff${randNum}`,
+    password: "password"
+  });
+
+  expect(res.data.message).toBe("success");
+
+  res = await axios.post(`${url}/api/account/login`, {
+    "username": `namejeff${randNum}`,
+    "password": "password"
+  });
+
+  expect(res.data.message).toBe("verify email");
+
+  let token = jwt.sign({ userId: `namejeff${randNum}` }, config.server.secret);
+
+  res = await axios.post(`${url}/api/snippet/create`, {
+    token: token,
+    imageURL: "image"
+  });
+
+  expect(res.data.message).toBe("success");
+
+  res = await axios.post(`${url}/api/snippet/deleteSnippet`, {
+    _id: "607c94b717e587002cabadfd" 
   });
 
   expect(res.data.message).toBe("success");
@@ -83,8 +165,34 @@ test('Delete snippet', async () => {
 test('Update score', async () => {
   let randNum = Math.ceil(Math.random() * 99999);
 
-  let res = await axios.post(`${url}/api/snippet/updateScore`, {
-    imageURL: "image.jpeg"
+  let res = await axios.post(`${url}/api/account/signup`, {
+    firstName: "name",
+    lastName: "jeff",
+    email: `namejeff${randNum}@mailinator.com`,
+    username: `namejeff${randNum}`,
+    password: "password"
+  });
+
+  expect(res.data.message).toBe("success");
+
+  res = await axios.post(`${url}/api/account/login`, {
+    "username": `namejeff${randNum}`,
+    "password": "password"
+  });
+
+  expect(res.data.message).toBe("verify email");
+
+  let token = jwt.sign({ userId: `namejeff${randNum}` }, config.server.secret);
+
+  res = await axios.post(`${url}/api/snippet/create`, {
+    token: token,
+    imageURL: "image"
+  });
+
+  expect(res.data.message).toBe("success");
+
+  res = await axios.post(`${url}/api/snippet/updateScore`, {
+    imageURL: "image"
   });
 
   expect(res.data.message).toBe("success");
