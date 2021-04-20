11fe568e0a772dc592b9d576f87ca413e4d5b6d9
Joe Brashear <joebrashear@live.com>
2021-04-20 13:15:55 -0400

    Connected swiping API, will need multiple accounts with snippets in order to finish this feature

--- app/lib/swiping.dart
+++ app/lib/swiping.dart
@@ -1,6 +1,15 @@
+import 'dart:convert';
+
 import 'package:alert_dialog/alert_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import './view-account.dart';
+import 'globals.dart';
+import 'models/snippet.dart';
+import 'models/userInfo.dart';
+import 'package:provider/provider.dart';
+import 'package:stack/stack.dart' as stack;
+
 
 class SwipingPage extends StatefulWidget {
   @override
@@ -8,8 +17,58 @@ class SwipingPage extends StatefulWidget {
 }
 
 class _SwipingPageState extends State<SwipingPage> {
+  stack.Stack<Snippet> hotSnippet = stack.Stack();
+  UserInfo userInfo;
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    userInfo = context.read<UserInfo>();
+    getRandomSnippet();
+  }
+
+  void getRandomSnippet() async {
+    
+    final url = Uri.parse('${Globals.apiUrl}/api/snippet/get-random');
+    var response = await get(url,
+      headers: { "Content-Type": "application/json" }
+    );
+
+    var resObj = json.decode(response.body);
+    if (response.statusCode != 200) {
+      String err = resObj["message"];
+      alert(context, title: Text('${response.statusCode}'), content: Text('$err'));
+      return;
+    }
+    print(resObj['message']);
+
+    if (resObj['message'] == 'success') {
+      setState(() {
+          hotSnippet.push(Snippet.fromJson(resObj["snippet"]));
+          loading = false;
+      });
+    } else {
+      return alert(context, content: Text(resObj['message']));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (loading) {
+      return Scaffold(
+        body: Align(
+          alignment: Alignment.center,
+          child: SizedBox(
+            width: 80,
+            height: 80,
+            child: CircularProgressIndicator(
+              strokeWidth: 8,
+            ),
+          ),
+        ),
+      );
+    }
     return Scaffold(
       body: Center(
         child: Column(
@@ -34,17 +93,15 @@ class _SwipingPageState extends State<SwipingPage> {
               ),
             ),
             SizedBox(height: 20),
-            Container(
-              child: new Image.asset("assets/bogosort.png",
-                  width: 350, height: 400, fit: BoxFit.contain),
-            ),
+            // API here, need to replace with new photo everytime a widget is clicked, Needs to be initialized?
+            codeSnippet(snippet: hotSnippet.pop()),
             SizedBox(height: 13),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 GestureDetector(
                   onTap: () async {
-                    return alert(context, title: Text("Trash!"));
+                    getRandomSnippet();
                   },
                   child: Container(
                     child: new Image.asset("assets/image 15.png",
@@ -54,7 +111,7 @@ class _SwipingPageState extends State<SwipingPage> {
                 SizedBox(width: 200),
                 GestureDetector(
                   onTap: () async {
-                    return alert(context, title: Text("Fire!"));
+                    getRandomSnippet();
                   },
                   child: Container(
                     child: new Image.asset("assets/fire 2.png",
@@ -67,6 +124,25 @@ class _SwipingPageState extends State<SwipingPage> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget codeSnippet({@required Snippet snippet}) {
+    return 
+        Expanded(
+            child: Container(
+              width: 300,
+              height: 500,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    snippet.imageURL,
+                  ),
+                  fit: BoxFit.cover,
+                  alignment: Alignment.topCenter,
+                )
+              ),
+            ),
     );
   }
 }
